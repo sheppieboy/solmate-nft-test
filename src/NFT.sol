6250c880c7af08dad810484f8e216011ea218c9f
Luke Sheppard <sheppiegit@gmail.com>
2023-07-04 17:27:48 -0400

    Added the NFT constructor.

--- src/NFT.sol
+++ src/NFT.sol
@@ -3,3 +3,18 @@ pragma solidity 0.8.10;
 
 import "solmate/tokens/ERC721.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol";
+
+contract NFT is ERC721 {
+    //storage
+    uint256 public currentTokenId;
+
+    //constructor
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
+
+    //state changing functions
+
+    //view functions
+}
