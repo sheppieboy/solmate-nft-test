a92f74a9fddddb0410f089569716453788679e1a
Luke Sheppard <sheppiegit@gmail.com>
2023-07-04 17:40:41 -0400

    Added some metadata and mint price, max supply.

--- src/NFT.sol
+++ src/NFT.sol
@@ -3,16 +3,24 @@ pragma solidity 0.8.10;
 
 import "solmate/tokens/ERC721.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract NFT is ERC721 {
+contract NFT is ERC721, Ownable {
+    using Strings for uint256;
     //storage
+    string public baseURI;
     uint256 public currentTokenId;
+    uint256 public constant TOTAL_SUPPLY = 10_000;
+    uint256 public constant MINT_PRICE = 0.08 ether;
 
     //constructor
     constructor(
         string memory _name,
-        string memory _symbol
-    ) ERC721(_name, _symbol) {}
+        string memory _symbol,
+        string memory _baseURI
+    ) ERC721(_name, _symbol) {
+        baseURI = _baseURI;
+    }
 
     //state changing functions
     function mintTo(address recipient) public payable returns (uint256) {
