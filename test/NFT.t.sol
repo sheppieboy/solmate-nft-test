cc11d0afafe5b88fe73f239aed303ad8c80eca80
Luke Sheppard <sheppiegit@gmail.com>
2023-07-05 14:21:00 -0400

    Added the initial tests that need to be implemented.

--- test/NFT.t.sol
+++ test/NFT.t.sol
@@ -13,4 +13,25 @@ contract NFTTest is Test {
         //deploy nft
         nft = new NFT("Test_NFT", "TEST", "bareURI");
     }
+
+    //Tests for mintTo function
+    function test_RevertMintWithoutValue() public {}
+
+    function test_MintPricePaid() public {}
+
+    function test_RevertMintMaxSupplyReached() public {}
+
+    function test_RevertMintToZeroAddress() public {}
+
+    function test_NewMintOwnerRegistered() public {}
+
+    function test_BalanceIncremented() public {}
+
+    function test_SafeContractReciever() public {}
+
+    function test_RevertUnSafeContractReceiver() public {}
+
+    function test_WithdrawalWorksAsOwner() public {}
+
+    function test_WithdrawalFailsAsNotOwner() public {}
 }
